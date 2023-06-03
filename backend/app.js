47e74678530034a7f256bc1b4cdd7c6d34b11cfe
Nebojsa <nebojsal@examroom.ai>
2023-06-03 14:19:05 +0200

    ++

--- backend/app.js
+++ backend/app.js
@@ -1,43 +1,45 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
 
-const app = express()
+const Post = require('./models/post');
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+const app = express();
+mongoose
+  .connect('mongodb+srv://lazarevicnebojsa1005:JRH2FW1hye45NrL3@cluster0.svuynwh.mongodb.net/node-angular?retryWrites=true&w=majority')
+  .then(() => console.log('connected to database'))
+  .catch(err => console.log('error', err));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-    next()
-})
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  next();
+});
 
 app.post('/api/posts', (req, res, next) => {
-    const post  = req.body
-    console.log(post)
-    res.status(201).json({
-        message: 'Post added successfully'
-    })
-})
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  console.log(post);
+  post.save();
+  res.status(201).json({
+    message: 'Post added successfully',
+  });
+});
 
 app.get('/api/posts', (req, res) => {
-    const posts = [
-        {
-            id: '12341',
-            title: 'First Server-side post',
-            content: 'This is a post'
-        },
-        {
-            id: '5434RASD',
-            title: 'Second Server-side post',
-            content: 'This is a post!'
-        },
-    ]
+  Post.find().then(posts => {
     res.status(200).json({
         message: 'Posts fetched successfully',
         posts,
-    })
-})
+      });
+  });
+ 
+});
 
 module.exports = app
